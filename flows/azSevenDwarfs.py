0b5144255f59e26eb541dc2697f8fbbda3a9ef27
Joe Hall <jhall@phdata.io>
2022-01-18 23:53:29 -0500

    cleanup etl; template pipeline

--- flows/azSevenDwarfs.py
+++ flows/azSevenDwarfs.py
@@ -5,10 +5,7 @@ from prefect import task, Flow
 
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
-#from azure.storage.blob import BlobServiceClient
 from azure.storage.filedatalake import DataLakeServiceClient
-# from azure.core._match_conditions import MatchConditions
-# from azure.storage.filedatalake._models import ContentSettings
 
 from flow_utils.prefect_configs import set_run_config,set_storage
 
@@ -24,23 +21,20 @@ def extract_data(url):
         raise ValueError("{} could not be retrieved.".format(url))
 
 @task(log_stdout=True)
-def transform(ref_data):
-    print("cleaning & transform aircraft data...")
-    live_aircraft_data = []
-    return live_aircraft_data
-
-@task(log_stdout=True)
 def load_reference_data(ref_data, az_credential):
     print("saving reference data...")
 
     storage_account_name = "workflowpoc"
+    
     service_client = DataLakeServiceClient(account_url="{}://{}.dfs.core.windows.net".format(
         "https", storage_account_name), credential=az_credential)
     file_system_client = service_client.get_file_system_client(file_system="raw")
     directory_client = file_system_client.get_directory_client("sevendwarfs")
     ts = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    
     filename = f"7_dwarfs_train_{ts}.csv"
     print(f"Uploading file {filename}")
+
     file_client = directory_client.get_file_client(filename)
     file_client.upload_data(ref_data, overwrite=True)
 
