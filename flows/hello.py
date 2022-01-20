d57a71fea4f1e6440d06d9e5769624d64842e834
Joe Hall <jhall@phdata.io>
2022-01-20 15:23:57 -0500

    update hello flow

--- flows/hello.py
+++ flows/hello.py
@@ -1,11 +1,18 @@
 import prefect
 from prefect import task, Flow
+from flow_utils.prefect_configs import set_run_config,set_storage
 
 @task(log_stdout=True)
 def hello_task():
     logger = prefect.context.get("logger")
     logger.info("Hello world!")
     return True
-
-with Flow("hello-flow") as flow:
+FLOW_NAME = "hello-flow"    
+with Flow(
+    FLOW_NAME,
+    #executor=LocalDaskExecutor(),
+    storage=set_storage(FLOW_NAME),
+    run_config=set_run_config(),
+) as flow:
+#with Flow("hello-flow") as flow:
     success = hello_task()
